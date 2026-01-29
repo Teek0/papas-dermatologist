8275d03fe4ad64a49a3f0a9e7417814c9dd36ca2
Damián Ibarra Z. <damian.ibarra@ug.uchile.cl>
2026-01-29 20:22:51 -0300

    Fade-in / Fade-out
    Customers now fade in and out when spawning and despawning

--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -13,15 +13,63 @@ public class CustomerManager : MonoBehaviour
     private void SpawnCustomer()
     {
         currentCustomer = new Customer(Constants, BodyOptions, MouthOptions, EyesOptions);
+
+        // Get SpriteRenderers for customer parts
+        SpriteRenderer bodyRenderer = (SpriteRenderer)transform.Find("Body").GetComponent(typeof(SpriteRenderer));
+        SpriteRenderer mouthRenderer = (SpriteRenderer)transform.Find("Mouth").GetComponent(typeof(SpriteRenderer));
+        SpriteRenderer eyesRenderer = (SpriteRenderer)transform.Find("Eyes").GetComponent(typeof(SpriteRenderer));
+
+        // Sprite color modifier
+        customerAlpha = 0f;
+        Color opacityColor = new Color(1f, 1f, 1f, customerAlpha);
+
+        // Make invisible before spawining
+        bodyRenderer.color = opacityColor;
+        mouthRenderer.color = opacityColor;
+        eyesRenderer.color = opacityColor;
+
         // Assigns randomized sprites
-        ((SpriteRenderer)transform.Find("Body").GetComponent(typeof(SpriteRenderer))).sprite = currentCustomer.GetBody();
-        ((SpriteRenderer)transform.Find("Mouth").GetComponent(typeof(SpriteRenderer))).sprite = currentCustomer.GetMouth();
-        ((SpriteRenderer)transform.Find("Eyes").GetComponent(typeof(SpriteRenderer))).sprite = currentCustomer.GetEyes();
+        bodyRenderer.sprite = currentCustomer.GetBody();
+        mouthRenderer.sprite = currentCustomer.GetMouth();
+        eyesRenderer.sprite = currentCustomer.GetEyes();
 
         // ToDo: Save as Prefab to export to second Scene
     }
 
+    private void FadeIn()
+    {
+        if (customerAlpha < 1f)
+        {
+            customerAlpha += 0.005f;
+            ((SpriteRenderer)transform.Find("Body").GetComponent(typeof(SpriteRenderer))).color = new Color(1f, 1f, 1f, customerAlpha);
+            ((SpriteRenderer)transform.Find("Mouth").GetComponent(typeof(SpriteRenderer))).color = new Color(1f, 1f, 1f, customerAlpha);
+            ((SpriteRenderer)transform.Find("Eyes").GetComponent(typeof(SpriteRenderer))).color = new Color(1f, 1f, 1f, customerAlpha);
+        }
+        else
+        {
+            attending = true;
+        }
+    }
+
+    private void FadeOut()
+    {
+        if (customerAlpha > 0f)
+        {
+            customerAlpha -= 0.005f;
+            ((SpriteRenderer)transform.Find("Body").GetComponent(typeof(SpriteRenderer))).color = new Color(1f, 1f, 1f, customerAlpha);
+            ((SpriteRenderer)transform.Find("Mouth").GetComponent(typeof(SpriteRenderer))).color = new Color(1f, 1f, 1f, customerAlpha);
+            ((SpriteRenderer)transform.Find("Eyes").GetComponent(typeof(SpriteRenderer))).color = new Color(1f, 1f, 1f, customerAlpha);
+        } else
+        {
+            attending = false;
+        }
+    }
+
+    private float customerAlpha;
+
     private bool attending;
+    private bool customerSpawned;
+
     private float timeToNextPatient;
     private float waitingTime;
 
@@ -30,6 +78,8 @@ public class CustomerManager : MonoBehaviour
     void Start()
     {
         attending = false;
+        customerSpawned = false;
+        customerAlpha = 0f;
         timeToNextPatient = Random.Range(3, Constants.MaxWaitingTime);
         waitingTime = 0;
     }
@@ -39,20 +89,45 @@ public class CustomerManager : MonoBehaviour
     {
         if(!attending)
         {
-            waitingTime += Time.deltaTime;
-            if(waitingTime >= timeToNextPatient)
+            // Not attending a customer
+
+            if(!customerSpawned)
             {
-                SpawnCustomer();
-                waitingTime = 0;
-                attending = true;
+                // Waits for the next customer to arrive
+                if (waitingTime >= timeToNextPatient)
+                {
+                    SpawnCustomer();
+                    customerSpawned = true;
+                    waitingTime = 0;
+                }
+                else
+                {
+                    waitingTime += Time.deltaTime;
+                    timeToNextPatient = Random.Range(3, Constants.MaxWaitingTime);
+                }
+            } else
+            {
+                // Customer fade-in
+                FadeIn();
             }
         } else
         {
-            waitingTime += Time.deltaTime;
-            if(waitingTime >= timeToNextPatient)
+            // Attending a customer
+
+            // This is placeholder to test fade-in/fade-out and customer randomization
+            if(customerSpawned)
             {
-                waitingTime = 0;
-                attending = false;
+                waitingTime += Time.deltaTime;
+                Debug.Log("Waiting Time: " + waitingTime + "; Max Time to Wait: " + timeToNextPatient);
+                Debug.Log("alpha = " + customerAlpha);
+                if (waitingTime >= timeToNextPatient)
+                {
+                    customerSpawned = false;
+                    waitingTime = 0;
+                }
+            } else
+            {
+                FadeOut();
             }
         }
     }
