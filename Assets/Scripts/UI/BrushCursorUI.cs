4740db2184461ae1a9629c5f6f6aa99e0b27e2a8
Nicole Sáez <satrin.nicole@gmail.com>
2026-07-10 22:18:20 -0400

    brush commit

--- Assets/Scripts/UI/BrushCursorUI.cs
+++ Assets/Scripts/UI/BrushCursorUI.cs
@@ -6,35 +6,77 @@ public class BrushCursorUI : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
     [SerializeField] private Image circleImage;
+    [SerializeField] private Image toolImage;
     [SerializeField] private CreamSelectionManager selectionManager;
     [SerializeField] private GameController gameController;
 
     [Header("Size")]
     [SerializeField] private float baseSize = 48f;
 
+    [Header("Cursor Sprites")]
+    [SerializeField] private Sprite acneCursorSprite;
+    [SerializeField] private Sprite wrinklesCursorSprite;
+    [SerializeField] private Sprite scarsCursorSprite;
+    [SerializeField] private Sprite eraserCursorSprite;
+
+    [Header("Cursor Placement")]
+    [SerializeField] private Vector2 creamCursorSize = new Vector2(64f, 64f);
+    [SerializeField] private Vector2 creamCursorHotspot = new Vector2(0.15f, 0.15f);
+    [SerializeField] private Vector2 eraserCursorSize = new Vector2(64f, 64f);
+    [SerializeField] private Vector2 eraserCursorHotspot = new Vector2(0.5f, 0.5f);
+
+    [Header("Cursor Tint")]
+    [SerializeField] private bool tintCreamCursorWithCreamColor;
+    [SerializeField] private Color eraserCursorTint = Color.white;
+
+    [Header("Precision Circle")]
+    [SerializeField] private bool showPrecisionCircle = true;
+    [SerializeField] private Vector2 precisionCircleSize = new Vector2(20f, 20f);
+    [SerializeField, Range(0f, 1f)] private float creamPrecisionCircleAlpha = 0.45f;
+    [SerializeField] private Color eraserPrecisionCircleColor = new Color(1f, 1f, 1f, 0.45f);
+
     private RectTransform rt;
+    private RectTransform circleRt;
+    private RectTransform toolRt;
     private Camera uiCamera;
     private Vector2 lastGoodPos;
-    private Vector2 lastSize;
-    private Color lastColor;
-    private bool lastEnabled;
+    private Vector2 lastToolSize;
+    private Vector2 lastHotspot;
+    private Vector2 lastCircleSize;
+    private Color lastToolColor;
+    private Color lastCircleColor;
+    private Sprite lastToolSprite;
+    private bool visualsEnabled;
 
     private void Awake()
     {
         rt = GetComponent<RectTransform>();
 
-        if (circleImage == null) circleImage = GetComponent<Image>();
+        Image rootImage = GetComponent<Image>();
+        if (toolImage == null && rootImage != null && rootImage != circleImage)
+            toolImage = rootImage;
+
+        if (circleImage == null)
+            circleImage = GetComponentInChildren<Image>(true);
+
         if (canvas == null) canvas = GetComponentInParent<Canvas>(true);
+        if (toolImage != null) toolRt = toolImage.rectTransform;
+        if (circleImage != null) circleRt = circleImage.rectTransform;
 
         if (selectionManager == null) selectionManager = FindFirstObjectByType<CreamSelectionManager>();
         if (gameController == null) gameController = FindFirstObjectByType<GameController>();
 
         ConfigureAsVisualOnlyCursor();
         ResolveUICamera();
+
         lastGoodPos = rt.anchoredPosition;
-        lastSize = rt.sizeDelta;
-        lastColor = circleImage != null ? circleImage.color : Color.white;
-        lastEnabled = circleImage != null && circleImage.enabled;
+        lastToolSize = rt.sizeDelta;
+        lastHotspot = Vector2.one * 0.5f;
+        lastCircleSize = circleRt != null ? circleRt.sizeDelta : precisionCircleSize;
+        lastToolColor = toolImage != null ? toolImage.color : Color.white;
+        lastCircleColor = circleImage != null ? circleImage.color : Color.white;
+        lastToolSprite = toolImage != null ? toolImage.sprite : null;
+        visualsEnabled = IsAnyVisualEnabled();
     }
 
     private void OnEnable()
@@ -49,9 +91,8 @@ public class BrushCursorUI : MonoBehaviour
         foreach (var graphic in graphics)
             graphic.raycastTarget = false;
 
-        var rootImage = GetComponent<Image>();
-        if (rootImage != null && rootImage != circleImage)
-            rootImage.enabled = false;
+        if (rt != null)
+            rt.pivot = Vector2.one * 0.5f;
     }
 
     private void ResolveUICamera()
@@ -64,14 +105,14 @@ public class BrushCursorUI : MonoBehaviour
 
     private void Update()
     {
-        if (canvas == null || rt == null || circleImage == null)
+        if (canvas == null || rt == null)
             return;
 
         bool canPaint = (gameController == null) ? true : gameController.CanPaint;
-        if (canPaint != lastEnabled)
+        if (canPaint != visualsEnabled)
         {
-            circleImage.enabled = canPaint;
-            lastEnabled = canPaint;
+            SetVisualsEnabled(canPaint);
+            visualsEnabled = canPaint;
         }
         if (!canPaint) return;
 
@@ -82,31 +123,168 @@ public class BrushCursorUI : MonoBehaviour
             out var localPos
         );
 
+        Vector2 pointerPos = ok ? localPos : lastGoodPos;
         if (ok)
-        {
-            rt.anchoredPosition = localPos;
             lastGoodPos = localPos;
-        }
-        else
-        {
-            rt.anchoredPosition = lastGoodPos;
-        }
 
-        if (selectionManager != null)
+        UpdateCursorVisual();
+        PositionCursorAtHotspot(pointerPos);
+    }
+
+    private void SetVisualsEnabled(bool enabled)
+    {
+        if (toolImage != null)
+            toolImage.enabled = enabled && GetCurrentSprite() != null;
+
+        if (circleImage != null)
+            circleImage.enabled = enabled && showPrecisionCircle;
+    }
+
+    private bool IsAnyVisualEnabled()
+    {
+        bool toolEnabled = toolImage != null && toolImage.enabled;
+        bool circleEnabled = circleImage != null && circleImage.enabled;
+        return toolEnabled || circleEnabled;
+    }
+
+    private void UpdateCursorVisual()
+    {
+        Sprite sprite = GetCurrentSprite();
+        if (toolImage != null)
         {
-            Color c = selectionManager.CurrentColor;
-            if (c != lastColor)
+            if (sprite != lastToolSprite)
             {
-                circleImage.color = c;
-                lastColor = c;
+                toolImage.sprite = sprite;
+                lastToolSprite = sprite;
+            }
+
+            bool shouldShowTool = sprite != null;
+            if (toolImage.enabled != shouldShowTool)
+                toolImage.enabled = shouldShowTool;
+
+            Color toolColor = GetCurrentToolTint();
+            if (toolColor != lastToolColor)
+            {
+                toolImage.color = toolColor;
+                lastToolColor = toolColor;
             }
         }
 
-        Vector2 size = new Vector2(baseSize, baseSize);
-        if (size != lastSize)
+        Vector2 toolSize = GetCurrentToolSize();
+        if (toolSize != lastToolSize)
         {
-            rt.sizeDelta = size;
-            lastSize = size;
+            rt.sizeDelta = toolSize;
+            if (toolRt != null)
+                toolRt.sizeDelta = toolSize;
+            lastToolSize = toolSize;
         }
+
+        Vector2 hotspot = GetCurrentHotspot();
+        if (hotspot != lastHotspot)
+        {
+            lastHotspot = hotspot;
+        }
+
+        UpdatePrecisionCircle(toolSize, hotspot);
+    }
+
+    private void PositionCursorAtHotspot(Vector2 pointerPos)
+    {
+        Vector2 toolSize = GetCurrentToolSize();
+        Vector2 hotspot = GetCurrentHotspot();
+        Vector2 hotspotOffset = new Vector2(
+            (hotspot.x - 0.5f) * toolSize.x,
+            (hotspot.y - 0.5f) * toolSize.y
+        );
+
+        rt.anchoredPosition = pointerPos - hotspotOffset;
+    }
+
+    private void UpdatePrecisionCircle(Vector2 toolSize, Vector2 hotspot)
+    {
+        if (circleImage == null || circleRt == null)
+            return;
+
+        if (circleImage.enabled != showPrecisionCircle)
+            circleImage.enabled = showPrecisionCircle;
+
+        Color circleColor = GetCurrentPrecisionCircleTint();
+        if (circleColor != lastCircleColor)
+        {
+            circleImage.color = circleColor;
+            lastCircleColor = circleColor;
+        }
+
+        if (precisionCircleSize != lastCircleSize)
+        {
+            circleRt.sizeDelta = precisionCircleSize;
+            lastCircleSize = precisionCircleSize;
+        }
+
+        circleRt.pivot = Vector2.one * 0.5f;
+        circleRt.anchoredPosition = new Vector2(
+            (hotspot.x - 0.5f) * toolSize.x,
+            (hotspot.y - 0.5f) * toolSize.y
+        );
+    }
+
+    private Sprite GetCurrentSprite()
+    {
+        if (selectionManager == null)
+            return toolImage != null ? toolImage.sprite : null;
+
+        if (selectionManager.IsEraserSelected)
+            return eraserCursorSprite != null ? eraserCursorSprite : (toolImage != null ? toolImage.sprite : null);
+
+        return selectionManager.CurrentCream switch
+        {
+            CreamType.Acne => acneCursorSprite != null ? acneCursorSprite : (toolImage != null ? toolImage.sprite : null),
+            CreamType.Wrinkles => wrinklesCursorSprite != null ? wrinklesCursorSprite : (toolImage != null ? toolImage.sprite : null),
+            CreamType.Scars => scarsCursorSprite != null ? scarsCursorSprite : (toolImage != null ? toolImage.sprite : null),
+            _ => toolImage != null ? toolImage.sprite : null
+        };
+    }
+
+    private Color GetCurrentToolTint()
+    {
+        if (selectionManager != null && selectionManager.IsEraserSelected)
+            return eraserCursorTint;
+
+        if (selectionManager != null && tintCreamCursorWithCreamColor)
+            return selectionManager.CurrentColor;
+
+        return Color.white;
+    }
+
+    private Color GetCurrentPrecisionCircleTint()
+    {
+        if (selectionManager != null && selectionManager.IsEraserSelected)
+            return eraserPrecisionCircleColor;
+
+        Color color = selectionManager != null ? selectionManager.CurrentColor : Color.white;
+        color.a = creamPrecisionCircleAlpha;
+        return color;
+    }
+
+    private Vector2 GetCurrentToolSize()
+    {
+        if (selectionManager != null && selectionManager.IsEraserSelected)
+            return eraserCursorSize;
+
+        if (creamCursorSize.x > 0f && creamCursorSize.y > 0f)
+            return creamCursorSize;
+
+        return new Vector2(baseSize, baseSize);
+    }
+
+    private Vector2 GetCurrentHotspot()
+    {
+        Vector2 hotspot = selectionManager != null && selectionManager.IsEraserSelected
+            ? eraserCursorHotspot
+            : creamCursorHotspot;
+
+        hotspot.x = Mathf.Clamp01(hotspot.x);
+        hotspot.y = Mathf.Clamp01(hotspot.y);
+        return hotspot;
     }
 }
